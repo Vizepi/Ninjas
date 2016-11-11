1b38aa616882529e787a0d43d5caa8b44c78ecc4
Akamis <mrtetex@gmail.com>
2016-11-11 13:37:11 +0100

    Player controller

---
+++ UnityProject/Assets/Scripts/Hitbox_ennemy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hitbox_ennemy : MonoBehaviour {
+	private BoxCollider2D hitbox;
+	private GameObject ennemy;
+	private Acrocatic.Player player;
+
+	// Use this for initialization
+	void Start () {
+		hitbox = GetComponent<BoxCollider2D>();
+		player = GetComponent<Acrocatic.Player> ();
+		ennemy = transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay2D (Collider2D other) {
+		if (Input.GetKeyDown ("a")) {
+			Destroy (ennemy.gameObject);
+		}
+	}
+}
