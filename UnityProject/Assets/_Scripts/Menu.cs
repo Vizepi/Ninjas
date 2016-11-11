001283834802bcf7a4947c6a20384fdbd5aeb413
Coco All <corentin.all@gmail.com>
2016-11-11 11:26:30 +0100

    Initial Commit

---
+++ UnityProject/Assets/_Scripts/Menu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    public void LoadLevel(string lvl)
+    {
+        SceneManager.LoadScene(lvl);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
