2a570acc27eb6847b74f0cf08b1acef8f01848ad
Coco All <corentin.all@gmail.com>
2016-11-12 14:48:10 +0100

    Creating GameOver scene

---
+++ UnityProject/Assets/_Scripts/RestartLevel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class RestartLevel : MonoBehaviour {
+
+    public string levelName;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Wait(6));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator Wait (int nb){
+        yield return new WaitForSeconds(nb);
+        SceneManager.LoadScene(levelName);
+    }
+}
