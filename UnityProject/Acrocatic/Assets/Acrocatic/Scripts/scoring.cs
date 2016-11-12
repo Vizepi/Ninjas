1ff0598ce0fdb4bcaa9cce0a95f2db74fe621968
Akamis <mrtetex@gmail.com>
2016-11-12 09:49:56 +0100

    Project avec la maps, le game controlleur, et les ennemies

---
+++ UnityProject/Acrocatic/Assets/Acrocatic/Scripts/scoring.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scoring : MonoBehaviour {
+
+	[HideInInspector]
+	public int score;
+
+	private string text;
+	public float time_combo;
+	public float timer;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		text = score.ToString();
+		//GetComponent<Canvas> ().gameObject = text;
+
+	}
+	public void AddScore(string type){
+		if (type == "kill") {
+			score = score + 100;
+		}
+		if (type == "relique") {
+			score = score + 1000;
+		}
+	}
+
+	/*
+	public void Launchcombo(){
+		time_combo += Time.deltaTime;
+		while (time_combo >= timer) {
+			timer = time_combo;
+			}
+		}
+	*/
+	}
