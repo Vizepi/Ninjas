7606f54f41c160dd1c7b3b545d0e82411c2369e2
Coco All <corentin.all@gmail.com>
2016-11-13 11:23:19 +0100

    Adding PauseScript

---
+++ UnityProject/Assets/_Scripts/PauseMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+    private bool isPaused = false;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused == false)
+            {
+                isPaused = true;
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                isPaused = false;
+                Time.timeScale = 1f;  
+            } 
+        }
+	
+	}
+}
