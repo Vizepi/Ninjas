4b5fa46f2d73e4cb94f45cf832d84636abf54f2e
Vizepi <joseph.kieffer@hotmail.fr>
2020-03-21 18:49:35 +0100

    Added editor path helpers and editor asmdef

---
+++ UnityProject/Assets/editor/EditorPaths.cs
@@ -0,0 +1,42 @@
+﻿// Copyright 2020 J. KIEFFER - All Rights Reserved.
+using System.IO;
+using UnityEngine;
+
+#if UNITY_EDITOR
+namespace vzp {
+	public static class EditorPaths {
+		public const string kAssetPathName = "Assets";
+		public const string kResourcePath = "Assets/Resources/";
+
+		public static void EnsureResourcePath( string _subPath = "" ) {
+			string dataPath = Application.dataPath;
+			if ( dataPath.EndsWith( kAssetPathName ) ) {
+				dataPath = dataPath.Substring( 0, dataPath.Length - kAssetPathName.Length );
+			}
+			EnsurePathExists( dataPath + kResourcePath + _subPath );
+		}
+
+		public static void EnsurePathExists( string _fullPath ) {
+			Directory.CreateDirectory( _fullPath );
+		}
+
+		public static string GetResourcePath( string _relativePath ) {
+			string dataPath = Application.dataPath;
+			if ( dataPath.EndsWith( kAssetPathName ) ) {
+				dataPath = dataPath.Substring( 0, dataPath.Length - kAssetPathName.Length );
+			}
+			return dataPath + kResourcePath + _relativePath;
+		}
+
+		public static string GetResourceDirectory( string _relativePath ) {
+			string dataPath = Application.dataPath;
+			if ( dataPath.EndsWith( kAssetPathName ) ) {
+				dataPath = dataPath.Substring( 0, dataPath.Length - kAssetPathName.Length );
+			}
+			dataPath += kResourcePath + _relativePath;
+			string fileName = Path.GetFileName( dataPath );
+			return dataPath.Substring( 0, dataPath.Length - fileName.Length );
+		}
+	}
+}
+#endif
