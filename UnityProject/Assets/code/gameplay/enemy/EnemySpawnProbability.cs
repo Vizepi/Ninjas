6fd7d02d06384ddb9134d3dfd81fa5d44ad7fa86
Vizepi <joseph.kieffer@hotmail.fr>
2020-04-11 17:49:34 +0200

    [ENEMY]Added missing script for enemy archetype

---
+++ UnityProject/Assets/code/gameplay/enemy/EnemySpawnProbability.cs
@@ -0,0 +1,20 @@
+﻿// Copyright 2020 J. KIEFFER - All Rights Reserved.
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace vzp {
+	[Serializable]
+	public struct EnemySpawnProbability {
+		[Tooltip( "Time at which this probability takes place (s)" )]
+		public float time;
+		[Tooltip( "Probability of this type of enemy to spawn (points)" )]
+		public float probability;
+	}
+
+	public struct EnemySpawnProbabilityComparer : IComparer {
+		public int Compare( object x, object y ) {
+			return ( ( EnemySpawnProbability )x ).time.CompareTo( ( ( EnemySpawnProbability )y ).time );
+		}
+	}
+}
